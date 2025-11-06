c9232a310b69d24571d1a111dc41755e42663382
alihassanmostafa <alihassan.mostafa@gmail.com>
2025-11-06 23:24:38 +0200

    Update __manifest__.py

--- currency_exchange_rate_amount_/__manifest__.py
+++ currency_exchange_rate_amount_/__manifest__.py
@@ -1,27 +1,29 @@
-
 {
     'name': 'Sales/Invoice Total Amount Exchange',
-    'version': '1.2',
-    'summary': '',
+    'version': '17.0.1.0.0',
+    'summary': 'Add exchange rate and total amount fields to Sales Orders and Invoices',
     'author': "JD DEVS",
-    'sequence': 10,
-    'depends': ['base', 'sale', 'account', 'product'],
+    'license': 'LGPL-3',
+    'category': 'Sales',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'base',
+        'sale_management',  # replaces 'sale'
+        'account',
+        'product',
+    ],
     'data': [
         'views/views.xml',
     ],
     'assets': {
         'web.assets_backend': [
-            "currency_exchange_rate_amount_/static/src/js/tax_total_new_widget.js",
+            'currency_exchange_rate_amount_/static/src/js/tax_total_new_widget.js',
         ],
-
         'web.assets_qweb': [
-            "currency_exchange_rate_amount_/static/src/xml/total_tax_json_widget_template.xml",
-            "currency_exchange_rate_amount_/static/src/xml/tax_total_tmpl_inherit.xml",
+            'currency_exchange_rate_amount_/static/src/xml/total_tax_json_widget_template.xml',
+            'currency_exchange_rate_amount_/static/src/xml/tax_total_tmpl_inherit.xml',
         ],
     },
-    'installable': True,
-    'application': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
     'images': ['static/description/assets/screenshots/banner.png'],
 }
